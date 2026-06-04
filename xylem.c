499e0023ba4d691281089f99e0ce0141de6d9e1f
Lolonoazolo <suryank162005@gmail.com>
2026-06-05 00:47:42 +0530

    updated

--- xylem.c
+++ xylem.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 int main ()
 {
-    printf("hello");
-    
+    int num , rem , temp , summ , sumo , last ,first;
+    printf("Enter a number : ") ;
+    scanf("%d",&num) ;
+    temp = num ;
+    summ = 0 ;
+    sumo = 0 ;
+    last = temp%10 ;
+    temp = temp/10 ;
+    for ( ; temp>=10 ; temp=temp/10 )
+    { rem = temp%10 ;
+        summ = summ + rem ;
+    }
+    first = temp ;
+    sumo = first + last ;
+    if (sumo == summ)
+    { 
+        printf("%d is a xylem number \n",num) ;
+     }
+    else
+    {
+        printf("%d is not a xylem number \n",num) ;
+    }
+    return 0 ;
 }
